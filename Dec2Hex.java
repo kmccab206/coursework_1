a38a856d828fb4998dc45c1b81376f1e77323178
kmccab206 <kmccab206@caledonian.ac.uk>
2019-11-04 19:10:40 +0000

    Added do while

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -8,49 +8,51 @@ class Dec2Hex{
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         System.out.println("================= Decimal to Hexadecimal Converter ===============");
-        try{
-            switch (menu()) {
-                case 1:
-                    System.out.println("Please enter an Integer:");
-                    Arg1 = input.nextInt();
-                    if (Arg1 != 0) {
-                        if (args.length > 0) {
-                            try {
-                                Arg1 = Integer.parseInt(args[0]);
-                            } catch (NumberFormatException e) {
-                                System.err.println("Argument" + args[0] + " must be an integer.");
-                                System.exit(1);
+        do{
+            try{
+                switch (menu()) {
+                    case 1:
+                        System.out.println("Please enter an Integer:");
+                        Arg1 = input.nextInt();
+                        if (Arg1 != 0) {
+                            if (args.length > 0) {
+                                try {
+                                    Arg1 = Integer.parseInt(args[0]);
+                                } catch (NumberFormatException e) {
+                                    System.err.println("Argument" + args[0] + " must be an integer.");
+                                    System.exit(1);
+                                }
                             }
+                            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+                            int rem, num;
+                            num = Arg1;
+                            String hexadecimal = "";
+                            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+                            while (num != 0) {
+                                rem = num % 16;
+                                hexadecimal = ch[rem] + hexadecimal;
+                                num = num / 16;
+                            }
+
+                            System.out.println("Hexadecimal representation of " + Arg1 + " is : " + hexadecimal);
+                        }else{
+                            System.out.println(Arg1 + " is invalid, please try again");
                         }
-                        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-                        int rem, num;
-                        num = Arg1;
-                        String hexadecimal = "";
-                        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-                        while (num != 0) {
-                            rem = num % 16;
-                            hexadecimal = ch[rem] + hexadecimal;
-                            num = num / 16;
-                        }
-
-                        System.out.println("Hexadecimal representation of " + Arg1 + " is : " + hexadecimal);
-                    }else{
-                        System.out.println(Arg1 + " is invalid, please try again");
-                    }
-
-                    break;
-                case 2:
-                    System.out.println("Thank you, Goodbye");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid Choice Please Try again");
+                        break;
+                    case 2:
+                        System.out.println("Thank you, Goodbye");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Invalid Choice Please Try again");
+                }
+            }catch(NoSuchElementException e){
+                System.out.println("Error: Timeout on user input");
+                System.exit(0);
             }
-        }catch(NoSuchElementException e){
-            System.out.println("Timeout on user input");
-            System.exit(0);
-        }
+        }while (menu() == 1  || menu() == 2);
 
     }
     private static int menu() {
@@ -61,7 +63,7 @@ class Dec2Hex{
         /***************************************************/
 
         System.out.println("Please enter your choice and press enter:");
-        System.out.println("1 - Enter a Decimal to convert to Hexadecimal \n 2 - Quit");
+        System.out.println("1 - Enter a Decimal to convert to Hexadecimal \n2 - Quit");
 
         selection = input.nextInt();
         return selection;
